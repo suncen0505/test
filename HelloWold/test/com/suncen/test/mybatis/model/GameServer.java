fdcf8fb17bb00373e8bbabe69c5077c7db86d12c
suncen0505 <1019518984@qq.com>
2014-07-09 15:12:57 +0800

    first commit

---
+++ HelloWold/test/com/suncen/test/mybatis/model/GameServer.java
@@ -0,0 +1,21 @@
+package com.suncen.test.mybatis.model;
+
+public class GameServer {
+	 private int code;
+	    private String name;
+	    private int gameid;
+	   
+	    public int getCode() {
+	        return code;
+	    }
+	    
+	    public int getGameId()
+	    {
+		return gameid;
+		
+	    }
+
+	    public String getName() {
+	        return name;
+	    }
+}
